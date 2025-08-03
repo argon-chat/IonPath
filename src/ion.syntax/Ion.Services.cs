1628f7d6fc8552d30ca9a793bc655859f921fbce
Yuuki Wesp <ls-micro@ya.ru>
2025-08-03 20:02:38 +0300

    Add Ion service syntax parsing methods
    Added parsing methods for Ion service syntax including service method and modifiers.

---
+++ src/ion.syntax/Ion.Services.cs
@@ -0,0 +1,47 @@
+﻿namespace ion.syntax;
+
+using Pidgin;
+using static Pidgin.Parser;
+using static Pidgin.Parser<char>;
+
+public partial class IonParser
+{
+    public static Parser<char, IonMethodSyntax> ServiceMethod =>
+        Map(
+            (doc, attrs, pos, modifiers, name, parameters, returnType, _) =>
+                new IonMethodSyntax(name, modifiers.ToList(), parameters.ToList(), returnType.GetValueOrDefault())
+                    .WithAttributes(attrs.ToList())
+                    .WithComments(doc)
+                    .WithPos(pos),
+            LeadingDoc.Labelled("doc"),
+            Attributes.Labelled("attributes"),
+            CurrentPos.Labelled("currentPos"),
+            MethodModifiers.Labelled("mod"),
+            Identifier.Labelled("identifier"),
+            ArgList.Labelled("args"),
+            Char(':').Before(SkipWhitespaces).Then(Type.Labelled("returnType")).Optional(),
+            Char(';').Before(SkipWhitespaces)
+        );
+
+    private static Parser<char, IonMethodModifiers> MethodModifierOne =>
+        OneOf(
+            String("stream").ThenReturn(IonMethodModifiers.Stream),
+            String("unary").ThenReturn(IonMethodModifiers.Unary),
+            String("internal").ThenReturn(IonMethodModifiers.Internal)
+        ).Before(SkipWhitespaces);
+
+    private static Parser<char, IEnumerable<IonMethodModifiers>> MethodModifiers =>
+        MethodModifierOne.Many();
+
+    public static Parser<char, IonServiceSyntax> Service =>
+        Map(
+            (doc, attrs, pos, name, parameters, methods) =>
+                new IonServiceSyntax(name, parameters.ToList(), methods.ToList()).WithAttributes(attrs.ToList()).WithPos(pos).WithComments(doc),
+            LeadingDoc,
+            Attributes,
+            CurrentPos,
+            String("service").Before(SkipWhitespaces).Then(Identifier),
+            ArgList,
+            ServiceMethod.Many().Between(Char('{').Before(SkipWhitespaces), Char('}').Before(SkipWhitespaces))
+        );
+}
