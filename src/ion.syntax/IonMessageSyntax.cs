f2b8eff40f919b09912ec3269e59a70904d82d3d
Yuuki Wesp <ls-micro@ya.ru>
2025-08-03 20:02:42 +0300

    Refactor syntax records and add service syntax support
    Added new record types for syntax representation and included support for service syntax in file processing. Updated position handling in syntax records.

--- src/ion.syntax/IonMessageSyntax.cs
+++ src/ion.syntax/IonMessageSyntax.cs
@@ -4,7 +4,9 @@ using Pidgin;
 
 public record IonSyntaxBase
 {
-    public SourcePos Position { get; set; }
+    public SourcePos StartPosition { get; set; }
+    public SourcePos? EndPosition { get; set; }
+    public FileInfo? SourceFile { get; set; }
 }
 
 public record IonSyntaxMember : IonSyntaxBase
@@ -13,31 +15,56 @@ public record IonSyntaxMember : IonSyntaxBase
     public List<IonAttributeSyntax> Attributes { get; set; } = [];
 }
 
-public record IonUnderlyingTypeSyntax(string Name, bool IsOptional, bool IsArray) : IonSyntaxBase;
+public record InvalidIonBlock(string block) : IonSyntaxMember
+{
 
-public record IonFieldSyntax(string Name, IonUnderlyingTypeSyntax Type) : IonSyntaxMember;
+}
 
-public record IonMessageSyntax(string Name, List<IonFieldSyntax> Fields) : IonSyntaxMember;
+public record IonIdentifier(string Identifier) : IonSyntaxBase()
+{
+    public static implicit operator IonIdentifier(string val) => new(val) { StartPosition = new SourcePos(0, 0) };
+}
+public record IonUnderlyingTypeSyntax(IonIdentifier Name, bool IsOptional, bool IsArray) : IonSyntaxBase;
 
-public record IonFlagEntrySyntax(string Name, string ValueExpression) : IonSyntaxBase;
+public record IonFieldSyntax(IonIdentifier Name, IonUnderlyingTypeSyntax Type) : IonSyntaxMember;
 
-public record IonFlagsSyntax(string Name, IonUnderlyingTypeSyntax Type, List<IonFlagEntrySyntax> Entries)
+public record IonMessageSyntax(IonIdentifier Name, List<IonFieldSyntax> Fields) : IonSyntaxMember;
+
+public record IonFlagEntrySyntax(IonIdentifier Name, string ValueExpression) : IonSyntaxBase;
+
+public record IonFlagsSyntax(IonIdentifier Name, IonUnderlyingTypeSyntax Type, List<IonFlagEntrySyntax> Entries)
     : IonSyntaxMember;
 
-public record IonEnumSyntax(string Name, IonUnderlyingTypeSyntax Type, List<IonFlagEntrySyntax> Entries)
+public record IonEnumSyntax(IonIdentifier Name, IonUnderlyingTypeSyntax Type, List<IonFlagEntrySyntax> Entries)
     : IonSyntaxMember;
 
-public record IonAttributeSyntax(string Name, List<string> Args) : IonSyntaxBase;
+public record IonAttributeSyntax(IonIdentifier Name, List<string> Args) : IonSyntaxBase;
 
 public record IonUseSyntax(string Path) : IonSyntaxMember;
 
 public record IonTypedefSyntax(IonUnderlyingTypeSyntax TypeName, IonUnderlyingTypeSyntax? BaseType) : IonSyntaxMember;
 
-public record IonArgumentSyntax(string argName, IonUnderlyingTypeSyntax type) : IonSyntaxMember;
+public record IonArgumentSyntax(IonIdentifier argName, IonUnderlyingTypeSyntax type) : IonSyntaxMember;
 
-public record IonAttributeDefSyntax(string Name, List<IonArgumentSyntax> Args) : IonSyntaxMember;
+public record IonAttributeDefSyntax(IonIdentifier Name, List<IonArgumentSyntax> Args) : IonSyntaxMember;
 
 public record IonFeatureSyntax(string featureName) : IonSyntaxMember;
+
+public record IonMethodSyntax(
+    IonIdentifier methodName,
+    List<IonMethodModifiers> modifiers,
+    List<IonArgumentSyntax> arguments,
+    IonUnderlyingTypeSyntax? returnType) : IonSyntaxMember;
+
+public record IonServiceSyntax(IonIdentifier serviceName, List<IonArgumentSyntax> BaseArguments, List<IonMethodSyntax> Methods)
+    : IonSyntaxMember;
+
+public enum IonMethodModifiers
+{
+    Unary,
+    Stream,
+    Internal
+}
 
 public record IonFileSyntax(
     string Name,
@@ -48,20 +75,53 @@ public record IonFileSyntax(
     List<IonEnumSyntax> enumSyntaxes,
     List<IonFlagsSyntax> flagsSyntaxes,
     List<IonMessageSyntax> messageSyntaxes,
-    List<IonTypedefSyntax> typedefSyntaxes)
+    List<IonTypedefSyntax> typedefSyntaxes,
+    List<IonServiceSyntax> serviceSyntaxes)
 {
     public List<IonSyntaxMember> Definitions => attributeDefSyntaxes
         .OfType<IonSyntaxMember>()
         .Concat(flagsSyntaxes)
         .Concat(enumSyntaxes)
-        .Concat(messageSyntaxes).Concat(typedefSyntaxes).ToList();
+        .Concat(messageSyntaxes)
+        .Concat(serviceSyntaxes)
+        .Concat(typedefSyntaxes).ToList();
 }
+
+public static class IonFileProcessingScope
+{
+    private static readonly ThreadLocal<FileInfo?> currentFile = new(true);
+
+    public static IDisposable Begin(FileInfo file)
+    {
+        if (currentFile.Value is not null) throw new InvalidOperationException($"Current thread already has locked file");
+        currentFile.Value = file;
+        return new Disposer();
+    }
+
+
+    private class Disposer : IDisposable
+    {
+        public void Dispose()
+        {
+            currentFile.Value = null;
+        }
+    }
+
+    internal static FileInfo? Take() => currentFile.Value;
+}
+
 
 public static class IonSyntaxEx
 {
     public static T WithComments<T>(this T t, string? comments) where T : IonSyntaxMember
     {
         t.Comments = comments;
+        return t;
+    }
+
+    public static T WithComments<T>(this T t, Maybe<string> comments) where T : IonSyntaxMember
+    {
+        t.Comments = comments.GetValueOrDefault();
         return t;
     }
 
@@ -73,7 +133,16 @@ public static class IonSyntaxEx
 
     public static T WithPos<T>(this T t, SourcePos pos) where T : IonSyntaxBase
     {
-        t.Position = pos;
+        t.StartPosition = pos;
+        t.SourceFile = IonFileProcessingScope.Take();
+        return t;
+    }
+
+    public static T WithPos<T>(this T t, SourcePos start, SourcePos end) where T : IonSyntaxBase
+    {
+        t.StartPosition = start;
+        t.EndPosition = end;
+        t.SourceFile = IonFileProcessingScope.Take();
         return t;
     }
 }
