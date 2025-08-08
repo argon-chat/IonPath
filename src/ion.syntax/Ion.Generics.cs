14b22e5e7517a84b9c370da2d5efdf1dc3f4ea45
Yuuki Wesp <ls-micro@ya.ru>
2025-08-09 02:43:00 +0300

    Add generic type parameter parsing to IonParser
    - Introduces methods for parsing generic type parameters in IonParser.

---
+++ src/ion.syntax/Ion.Generics.cs
@@ -0,0 +1,23 @@
+﻿namespace ion.syntax;
+
+using Pidgin;
+using static Pidgin.Parser;
+using static Pidgin.Parser<char>;
+
+public partial class IonParser
+{
+    public static Parser<char, IReadOnlyList<IonTypeParameterSyntax>> GenericParameterList =>
+        Char('<')
+            .Then(TypeParameterSyntax.Separated(Char(',').Then(SkipWhitespaces)))
+            .Before(Char('>')).Select(x => x.ToList()).OfType<IReadOnlyList<IonTypeParameterSyntax>>();
+
+    public static Parser<char, IonTypeParameterSyntax> TypeParameterSyntax =>
+        from startPos in CurrentPos
+        from name in Identifier.Before(SkipWhitespaces)
+        from constraints in Char(':')
+            .Then(SkipWhitespaces)
+            .Then(Type.Separated(Char(',').Then(SkipWhitespaces)))
+            .Optional()
+        from endPos in CurrentPos
+        select new IonTypeParameterSyntax(name).WithPos(startPos, endPos);
+}
