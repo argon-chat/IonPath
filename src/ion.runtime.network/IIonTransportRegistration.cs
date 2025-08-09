3f8595bb34c19f74b2eccf234b6dfb71f40e77df
Yuuki Wesp <ls-micro@ya.ru>
2025-08-09 17:52:35 +0300

    add IIonTransportRegistration with IonDescriptorRegistration

---
+++ src/ion.runtime.network/IIonTransportRegistration.cs
@@ -0,0 +1,20 @@
+﻿namespace ion.runtime.network;
+
+using Microsoft.Extensions.DependencyInjection;
+
+public interface IIonTransportRegistration
+{
+    IIonTransportRegistration AddService<TInterface, TImpl>()
+        where TInterface : class, IIonService
+        where TImpl : class, TInterface;
+}
+
+
+public readonly struct IonDescriptorRegistration(IServiceCollection col) : IIonTransportRegistration
+{
+    public IIonTransportRegistration AddService<TInterface, TImpl>() where TInterface : class, IIonService where TImpl : class, TInterface
+    {
+        col.AddIonService<TInterface, TImpl>();
+        return this;
+    }
+}
