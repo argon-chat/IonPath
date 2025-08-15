39e1cb082366c5e99a324c3167907faaabe128c9
Yuuki Wesp <ls-micro@ya.ru>
2025-08-16 01:05:28 +0300

    Refactor CompileCommand for improved file handling and generation
    - Added option to skip emitting .csproj files
    - Enhanced file search to include subdirectories
    - Improved error handling for missing .ion files

--- src/ionc/Commands/CompileCommand.cs
+++ src/ionc/Commands/CompileCommand.cs
@@ -1,17 +1,23 @@
 ﻿namespace ion.compiler.Commands;
 
 using CodeGen;
-using Microsoft.Build.Framework;
 using runtime;
+using Spectre.Console;
 using Spectre.Console.Cli;
 using syntax;
 using System.Collections.Generic;
 using System.Diagnostics;
-using Spectre.Console;
+using System.Numerics;
 
-public class CompileCommand : AsyncCommand
+public class CompileOptions : CommandSettings
 {
-    public override Task<int> ExecuteAsync(CommandContext context)
+    [CommandOption("-n|--no-emit-csproj")]
+    public bool NoEmitCsProj { get; set; }
+}
+
+public class CompileCommand : AsyncCommand<CompileOptions>
+{
+    public override Task<int> ExecuteAsync(CommandContext context, CompileOptions options)
     {
         var watch = Stopwatch.StartNew();
         var currentDir = new DirectoryInfo(Directory.GetCurrentDirectory());
@@ -28,7 +34,17 @@ public class CompileCommand : AsyncCommand
 
         var project = IonProjectConfig.FromJson(File.ReadAllText(projectFile.FullName));
 
-        var files = currentDir.EnumerateFiles("*.ion").ToList();
+        var files = currentDir.EnumerateFiles("*.ion", SearchOption.AllDirectories).ToList();
+
+
+        if (!files.Any())
+        {
+            IonDiagnosticRenderer.RenderDiagnostics([
+                new IonDiagnostic("ION", IonDiagnosticSeverity.Error,
+                    "Project 'ion.config.json' found, but no any *.ion files found.", new IonSyntaxBase())
+            ]);
+            return Task.FromResult(-1);
+        }
 
         var list = new List<IonFileSyntax>();
         foreach (var file in files)
@@ -56,18 +72,19 @@ public class CompileCommand : AsyncCommand
         Checks(ctx);
         new RestoreUnresolvedTypeStage(ctx).DoProcess();
         Checks(ctx);
-
         var graph = new IonDependencyGraph(ctx.ProcessedModules.Concat(ctx.GlobalModules));
         graph.Generate();
 
-        
+        if (!options.NoEmitCsProj)
+            IonCSharpGenerator.GenerateCsproj(project.Name, projectFile.Directory!.File($"{project.Name}.csproj"));
+        File.WriteAllText(projectFile.Directory!.File($"globals.cs").FullName, IonCSharpGenerator.GenerateGlobalTypes());
 
+        GenerateDefault(currentDir, project, ctx, projectFile.Directory!.Directory("models"));
 
         foreach (var generatorCfg in project.Generators)
         {
             if (generatorCfg.Platform is IonGeneratorPlatform.Go or IonGeneratorPlatform.Rust)
                 throw new NotSupportedException($"Platform {generatorCfg.Platform} currently is not support");
-
             if (generatorCfg.Type == IonGeneratorType.Server)
                 GenerateServer(currentDir, project, ctx, generatorCfg);
             if (generatorCfg.Type == IonGeneratorType.Client)
@@ -79,14 +96,9 @@ public class CompileCommand : AsyncCommand
         return Task.FromResult(0);
     }
 
-    private void GenerateClient(DirectoryInfo currentDir, IonProjectConfig project, CompilationContext ctx, IonGeneratorConfig generatorCfg)
+    private void GenerateDefault(DirectoryInfo currentDir, IonProjectConfig project, CompilationContext context, DirectoryInfo outputFolder)
     {
-        throw new NotImplementedException();
-    }
-
-    private void GenerateServer(DirectoryInfo currentDir, IonProjectConfig project, CompilationContext context, IonGeneratorConfig generatorCfg)
-    {
-        var outputDirectory = currentDir.Combine(generatorCfg.Output);
+        var outputDirectory = currentDir.Combine(outputFolder.FullName);
 
         if (!outputDirectory.Exists)
             outputDirectory.Create();
@@ -95,18 +107,44 @@ public class CompileCommand : AsyncCommand
 
         foreach (var file in outputDirectory.EnumerateFiles("*.cs")) file.Delete();
 
-        IonCSharpGenerator.GenerateCsproj(project.Name, outputDirectory.File($"{project.Name}.csproj"));
-        File.WriteAllText(outputDirectory.File($"globals.cs").FullName, IonCSharpGenerator.GenerateGlobalTypes());
-
         foreach (var module in context.ProcessedModules)
         {
             File.WriteAllText(outputDirectory.File($"{module.Name}.cs").FullName, generator.GenerateModule(module));
             File.WriteAllText(outputDirectory.File($"{module.Name}.formatters.cs").FullName, generator.GenerateAllFormatters(module.Definitions));
-            File.WriteAllText(outputDirectory.File($"{module.Name}.executors.cs").FullName, generator.GenerateAllServiceExecutors(module.Services));
         }
 
         File.WriteAllText(outputDirectory.File($"moduleInit.cs").FullName,
-            generator.GenerateModuleInit(context.ProcessedModules.SelectMany(x => x.Definitions).DistinctBy(x => x.name.Identifier), context.ProcessedModules.SelectMany(x => x.Services).DistinctBy(x => x.name.Identifier)));
+            generator.GenerateModuleInit(context.ProcessedModules.SelectMany(x => x.Definitions).DistinctBy(x => x.name.Identifier), 
+                context.ProcessedModules.SelectMany(x => x.Services).DistinctBy(x => x.name.Identifier).ToList(),
+                project.Generators.Any(x => x.Type == IonGeneratorType.Client),
+                project.Generators.Any(x => x.Type == IonGeneratorType.Server)));
+    }
+
+    private void GenerateClient(DirectoryInfo currentDir, IonProjectConfig project, CompilationContext context, IonGeneratorConfig generatorCfg)
+    {
+        var outputDirectory = currentDir.Combine(generatorCfg.Output);
+
+        if (!outputDirectory.Exists)
+            outputDirectory.Create();
+        var generator = new IonCSharpGenerator(project.Name);
+
+        foreach (var module in context.ProcessedModules)
+        {
+            File.WriteAllText(outputDirectory.File($"{module.Name}.clientImpls.cs").FullName, generator.GenerateAllServiceClientImpl(module.Services));
+        }
+    }
+
+    private void GenerateServer(DirectoryInfo currentDir, IonProjectConfig project, CompilationContext context, IonGeneratorConfig generatorCfg)
+    {
+        var outputDirectory = currentDir.Combine(generatorCfg.Output);
+        var generator = new IonCSharpGenerator(project.Name);
+        if (!outputDirectory.Exists)
+            outputDirectory.Create();
+
+        foreach (var module in context.ProcessedModules)
+        {
+            File.WriteAllText(outputDirectory.File($"{module.Name}.executors.cs").FullName, generator.GenerateAllServiceExecutors(module.Services));
+        }
     }
 
 
