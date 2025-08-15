84783d73d0c758cb4cf4a7e1e2ffaec8b7932222
Yuuki Wesp <ls-micro@ya.ru>
2025-08-16 01:05:02 +0300

    Add IonExecutorMetadataStorage class for managing server and client types with methods to add and retrieve executors.

---
+++ src/ion.runtime/IonExecutorMetadataStorage.cs
@@ -0,0 +1,36 @@
+﻿namespace ion.runtime;
+
+using ion.runtime.network;
+using Microsoft.Extensions.DependencyInjection;
+
+public static class IonExecutorMetadataStorage
+{
+    public static readonly Dictionary<string, Type> ServerTypes = new();
+    public static readonly Dictionary<string, Type> ClientTypes = new();
+    public static void AddExecutor<T>(string typeName) where T : IServiceExecutorRouter
+        => ServerTypes.Add(typeName, typeof(T));
+
+    public static void AddClient<T>(string typeName) where T : IIonService
+        => ClientTypes.Add(typeName, typeof(T));
+
+    public static IServiceExecutorRouter Take(string serviceName, AsyncServiceScope scope)
+    {
+        if (ServerTypes.TryGetValue(serviceName, out var type))
+            return (IServiceExecutorRouter)Activator.CreateInstance(type, scope)!;
+        throw new InvalidOperationException();
+    }
+
+    public static IIonService TakeClient(string serviceName, AsyncServiceScope scope, params object[] args)
+    {
+        if (ClientTypes.TryGetValue(serviceName, out var type))
+            return (IIonService)ActivatorUtilities.CreateInstance(scope.ServiceProvider, type, args)!;
+        throw new InvalidOperationException();
+    }
+
+    public static T TakeClient<T>(AsyncServiceScope scope, params object[] args) where T : IIonService
+    {
+        if (ClientTypes.TryGetValue(typeof(T).Name, out var type))
+            return (T)ActivatorUtilities.CreateInstance(scope.ServiceProvider, type, args)!;
+        throw new InvalidOperationException();
+    }
+}
--- src/ion.runtime/IonProtocolError.cs
+++ src/ion.runtime/IonProtocolError.cs
@@ -2,7 +2,10 @@
 
 using System.Formats.Cbor;
 
-public record struct IonProtocolError(string code, string msg);
+public record struct IonProtocolError(string code, string msg)
+{
+    public static IonProtocolError UPSTREAM_ERROR(string msg) => new("UPSTREAM_ERROR", msg);
+}
 
 public class IonProtocolErrorFormatter : IonFormatter<IonProtocolError>
 {
