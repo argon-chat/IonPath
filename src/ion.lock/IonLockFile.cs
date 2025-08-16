abd2e476ab8cad43145054960f73f14d234e8c99
Yuuki Wesp <ls-micro@ya.ru>
2025-08-16 03:40:47 +0300

    Add global type aliases and lock file configurations
    - Added global type aliases for various data types
    - Created ion.lock configuration files for runtime locking
    - Implemented IonLockFile class to read and write lock files
    - Defined message structures for LockFile, TypeEntity, FieldEntity, MethodEntity, ArgumentEntity, AttributeEntity
    - Generated formatters for serialization/deserialization of these entities

---
+++ src/ion.lock/IonLockFile.cs
@@ -0,0 +1,45 @@
+﻿namespace ion.runtime.locking;
+
+public class IonLockFile
+{
+    public static IonLockFile Open(FileInfo file)
+    {
+        ReadOnlyMemory<u1> bytes = File.ReadAllBytes(file.FullName);
+
+        var header = bytes.Span[..3];
+
+        var reader = new CborReader(bytes[4..]);
+
+        reader.ReadStartArray();
+        var r = IonFormatterStorage<LockFileEntity>.Read(reader);
+        reader.ReadEndArray();
+        return new();
+    }
+
+    public static unsafe void Write(IonLockFile entity, FileInfo file)
+    {
+        var writer = new CborWriter();
+
+
+
+
+        writer.WriteStartArray(null);
+
+        IonFormatterStorage<LockFileEntity>.Write(writer, new LockFileEntity(4, "123", "811", IonArray<u1>.Empty, IonArray<TypeEntity>.Empty));
+
+        writer.WriteEndArray();
+
+
+        Span<u1> span = stackalloc u1[writer.BytesWritten + 4];
+
+
+        span[0] = (u1)'I';
+        span[1] = (u1)'O';
+        span[2] = (u1)'N';
+        span[3] = (u1)'L';
+
+        writer.Encode(span[4..]);
+
+        File.WriteAllBytes(file.FullName, span);
+    }
+}
