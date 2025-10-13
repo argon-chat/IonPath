4aac1ec278ca9dc37a7b16b508ccb726a67a2fb7
Yuuki Wesp <ls-micro@ya.ru>
2025-10-14 02:51:55 +0300

    aot behavior

--- src/ion.runtime/IonExecutorMetadataStorage.cs
+++ src/ion.runtime/IonExecutorMetadataStorage.cs
@@ -1,19 +1,31 @@
 ﻿namespace ion.runtime;
 
-using network;
 using Microsoft.Extensions.DependencyInjection;
+using network;
+using System.Diagnostics.CodeAnalysis;
 
 public static class IonExecutorMetadataStorage
 {
+    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(IonExecutorMetadataStorage))]
     public static readonly Dictionary<string, Type> ServerTypes = new();
+    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(IonExecutorMetadataStorage))]
     public static readonly Dictionary<string, Type> ClientTypes = new();
-    public static void AddExecutor<T>(string typeName)
+    public static void AddExecutor<
+        [DynamicallyAccessedMembers(
+            DynamicallyAccessedMemberTypes.PublicConstructors |
+            DynamicallyAccessedMemberTypes.Interfaces)] T>
+        (string typeName)
     {
         if (typeof(T).GetInterfaces().Contains(typeof(IServiceExecutorRouter)) || typeof(T).GetInterfaces().Contains(typeof(IServiceStreamExecutorRouter)))
             ServerTypes.TryAdd(typeName, typeof(T));
     }
 
-    public static void AddClient<T>(string typeName) where T : IIonService 
+    public static void AddClient<
+        [DynamicallyAccessedMembers(
+            DynamicallyAccessedMemberTypes.PublicConstructors |
+            DynamicallyAccessedMemberTypes.Interfaces)] T
+    >(string typeName)
+        where T : IIonService
         => ClientTypes.TryAdd(typeName, typeof(T));
 
     public static IServiceExecutorRouter Take(string serviceName, AsyncServiceScope scope)
@@ -36,7 +48,10 @@ public static class IonExecutorMetadataStorage
         throw new InvalidOperationException();
     }
 
-    public static T TakeClient<T>(AsyncServiceScope scope, params object[] args) where T : IIonService
+    public static T TakeClient<
+        [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] T
+    >(AsyncServiceScope scope, params object[] args)
+        where T : IIonService
     {
         if (ClientTypes.TryGetValue(typeof(T).Name, out var type))
             return (T)ActivatorUtilities.CreateInstance(scope.ServiceProvider, type, args)!;
